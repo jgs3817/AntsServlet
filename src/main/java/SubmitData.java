1ed0a4574b0b5b26d193b8dc3ae31f1ead5923a7
jgs3817 <jgs3817@ic.ac.uk>
2019-12-05 09:44:46 +0000

    5/12/19 Added 2 fields to SubmitData

--- src/main/java/SubmitData.java
+++ src/main/java/SubmitData.java
@@ -3,8 +3,18 @@ import java.util.ArrayList;
 
 public class SubmitData implements Serializable {
     private ArrayList<ArrayList<Integer>> antData;
+    private String videoID;
+    private int frameID;
 
     public ArrayList<ArrayList<Integer>> antDataReturn() {
         return antData;
+    }
+
+    public String videoIDReturn(){
+        return videoID;
+    }
+
+    public int frameIDReturn(){
+        return frameID;
     }
 }
